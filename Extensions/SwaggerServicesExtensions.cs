d42c2700ea453330042188baa02b967ab436eff1
MohammadrezaBonyadi <bonyadi.n2000@gmail.com>
2025-08-11 05:17:23 +0330

    Add Swagger support and update dependencies
    Introduced SwaggerServicesExtensions to encapsulate Swagger service registration. Added Swashbuckle.AspNetCore packages for Swagger support and downgraded AutoMapper to version 12.0.1. Minor formatting fix in appsettings.Development.json.

---
+++ Extensions/SwaggerServicesExtensions.cs
@@ -0,0 +1,22 @@
+﻿namespace minutes90.Extensions
+{
+    public static class SwaggerServicesExtensions
+    {
+        /// <summary>
+        /// Adds Swagger services to the application's service collection.
+        /// </summary>
+        /// <param name="services">The service collection to which Swagger services will be added.</param>
+        /// <returns>The updated service collection.</returns>
+        public static IServiceCollection AddOpenApi(this IServiceCollection services)
+        {
+            // Adds support for API endpoint exploration and documentation.
+            services.AddEndpointsApiExplorer();
+
+            // Configures Swagger to generate API documentation.
+            services.AddSwaggerGen();
+
+            return services;
+        }
+    }
+}
+}
